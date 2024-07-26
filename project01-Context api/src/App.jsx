f886dcd79b80cfe2e45f4f393d33c8005a31b313
Chaitanya Kulkarni <147541263+CHAITANYAK2101@users.noreply.github.com>
2024-07-26 08:39:15 +0530

    Update App.jsx

--- project01-Context api/src/App.jsx
+++ project01-Context api/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <UserContextProvider>
-      <h1>React with chai and share is important</h1>
+      <h1>React Form Using Context Api</h1>
       <Login />
       <Profile />
     </UserContextProvider>
